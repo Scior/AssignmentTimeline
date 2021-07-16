18419b32a6c7f553a014baec3e08dfaaffe7960a
Suita Fujino <interval12@gmail.com>
2021-07-16 17:10:00 +0900

    Separate HTTPMethod from APIRequest

--- AssignmentTimeline/Data/Network/APIRequest.swift
+++ AssignmentTimeline/Data/Network/APIRequest.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
-
 protocol APIRequest {
     associatedtype Response: Decodable
     var url: String { get }
