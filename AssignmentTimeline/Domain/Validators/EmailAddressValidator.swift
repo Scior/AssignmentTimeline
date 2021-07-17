7efa6d5505a896df8a2684100921312c8bf7366f
Suita Fujino <interval12@gmail.com>
2021-07-17 15:40:40 +0900

    Separate Validator and ValidationError as files

--- AssignmentTimeline/Domain/Validators/EmailAddressValidator.swift
+++ AssignmentTimeline/Domain/Validators/EmailAddressValidator.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-/// Validator共通のI/Fを定義するプロトコル
-protocol Validator {
-    associatedtype Value
-    func validate(value: Value) -> Result<Value, ValidationError>
-}
-
-enum ValidationError: LocalizedError {
-    case notPassed(reason: String)
-
-    var errorDescription: String? {
-        switch self {
-        case let .notPassed(reason):
-            return "Validation failed because: \(reason)"
-        }
-    }
-}
-
 protocol EmailAddressValidatorProtocol {
     func validate(value: String) -> Result<String, ValidationError>
 }
