5c0e0f70e37fadb99d9c9f954b664859bf12bc12
Suita Fujino <interval12@gmail.com>
2021-07-17 15:39:45 +0900

    Add logic to EmailAddressValidator

--- AssignmentTimeline/Domain/Validators/EmailAddressValidator.swift
+++ AssignmentTimeline/Domain/Validators/EmailAddressValidator.swift
@@ -29,7 +29,28 @@ protocol EmailAddressValidatorProtocol {
 }
 
 struct EmailAddressValidator: Validator, EmailAddressValidatorProtocol {
+    enum Const {
+        /// RFC5322に近い正規表現パターン
+        /// See: http://emailregex.com/
+        static let regex: NSRegularExpression? = try? NSRegularExpression(
+            pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        )
+    }
+
+    /// Emailアドレスとして正しいか検証する.
+    /// - Parameter value: 検証される文字列
+    /// - Returns: 正しいなら`.success(value)`. 不正なら`.failure(ValidationError)`
     func validate(value: String) -> Result<String, ValidationError> {
-        return .failure(.notPassed(reason: ""))
+        guard let regex = Const.regex else {
+            assertionFailure("Invalid regex pattern")
+            // サーバーサイドのvalidationもあるので、もしパターンが不正でもここでは通す
+            return .success(value)
+        }
+
+        if regex.firstMatch(in: value, range: .init(location: 0, length: value.count)) != nil {
+            return .success(value)
+        } else {
+            return .failure(.notPassed(reason: ""))
+        }
     }
 }
