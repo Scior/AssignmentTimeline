b8e1e22a62d9d8b2b6c15a2af115cfe6123693e4
Suita Fujino <interval12@gmail.com>
2021-07-19 19:17:58 +0900

    Add test cases to TimelineRepositoryTests

--- AssignmentTimelineTests/Data/Repositories/Timeline/TimelineRepositoryTests.swift
+++ AssignmentTimelineTests/Data/Repositories/Timeline/TimelineRepositoryTests.swift
@@ -20,7 +20,7 @@ final class TimelineRepositoryTests: XCTestCase {
 
     // MARK: - Test cases
 
-    func testLoginWithResponse() {
+    func testGetTimelineWithResponse() {
         let response: TimelineResponse = [
             .init(
                 id: 10,
@@ -42,5 +42,38 @@ final class TimelineRepositoryTests: XCTestCase {
             response,
             publisher: timelineRepository.getTimeline(request: .init(accessToken: "abcde", page: 1))
         )
+    }
+
+    func testGetTimelineWithUnauthorizedError() {
+            let responseStatusCode = 401
+            apiClientMock.publisher = failAnyError(APIClient.NetworkError.failed(statusCode: responseStatusCode, data: .init()))
+
+            waitForPublishingError(publisher: timelineRepository.getTimeline(
+                request: .init(accessToken: "abcde", page: 1)
+            )) { error in
+                return error == .unauthorized
+            }
+    }
+
+    func testGetTimelineWithNotFoundError() {
+            let responseStatusCode = 404
+            apiClientMock.publisher = failAnyError(APIClient.NetworkError.failed(statusCode: responseStatusCode, data: .init()))
+
+            waitForPublishingError(publisher: timelineRepository.getTimeline(
+                request: .init(accessToken: "abcde", page: 1)
+            )) { error in
+                return error == .notFound
+            }
+    }
+
+    func testGetTimelineWithOtherError() {
+            let responseStatusCode = 500
+            apiClientMock.publisher = failAnyError(APIClient.NetworkError.failed(statusCode: responseStatusCode, data: .init()))
+
+            waitForPublishingError(publisher: timelineRepository.getTimeline(
+                request: .init(accessToken: "abcde", page: 1)
+            )) { error in
+                return error == .others
+            }
     }
 }
