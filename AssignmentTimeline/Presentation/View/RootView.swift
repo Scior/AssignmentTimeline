9990d29e7e5f23cd5a6701e669317e44d26be91f
Suita Fujino <interval12@gmail.com>
2021-07-20 23:02:53 +0900

    Add root view to show LoginView and TimelineVC

---
+++ AssignmentTimeline/Presentation/View/RootView.swift
@@ -0,0 +1,37 @@
+//
+//  RootView.swift
+//  AssignmentTimeline
+//
+//  Created by Fujino Suita on 2021/07/20.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct RootView: View {
+    let store: Store<AppState, AppAction>
+
+    var body: some View {
+        SwitchStore(self.store) {
+            CaseLet(state: /AppState.login, action: AppAction.login) { store in
+                LoginView(store: store)
+            }
+            CaseLet(state: /AppState.timeline, action: AppAction.timeline) { _ in
+                TimelineViewControllerRepresentable() // FIXME: inject store
+            }
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView(store: .init(
+            initialState: .login(.empty),
+            reducer: SharedReducers.app,
+            environment: AppEnvironment(
+                apiClient: APIClient.shared,
+                mainQueue: .main
+            )
+        ))
+    }
+}
