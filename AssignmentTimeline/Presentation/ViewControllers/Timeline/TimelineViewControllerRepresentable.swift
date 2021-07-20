cc7ba2d158344bee64df0cf76c2402dd36b6e8f3
Suita Fujino <interval12@gmail.com>
2021-07-20 23:03:54 +0900

    Wrap TimelineVC with UIViewControllerRepresntable

--- AssignmentTimeline/Presentation/ViewControllers/Timeline/TimelineViewControllerRepresentable.swift
+++ AssignmentTimeline/Presentation/ViewControllers/Timeline/TimelineViewControllerRepresentable.swift
@@ -5,4 +5,19 @@
 //  Created by Fujino Suita on 2021/07/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct TimelineViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> TimelineViewController {
+        return .init(store: .init(
+            initialState: .empty,
+            reducer: SharedReducers.timeline,
+            environment: TimelineEnvironment(
+                repository: TimelineRepository(dependency: .init(client: APIClient.shared)),
+                mainQueue: .main
+            )
+        ))
+    }
+
+    func updateUIViewController(_ uiViewController: TimelineViewController, context: Context) {}
+}
